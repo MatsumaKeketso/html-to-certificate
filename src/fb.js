cf4b82e977cfbd56f5cbc5e1522ef9be80ee078b
Keketso Matsuma <keketsomatsuma88@gmail.com>
2024-03-04 19:43:42 +0200

    fixed margin

---
+++ src/fb.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmgLPoIxGgw7NDJpGqmnWNUYW6nTjRI9U",
+  authDomain: "curriculum-template.firebaseapp.com",
+  projectId: "curriculum-template",
+  storageBucket: "curriculum-template.appspot.com",
+  messagingSenderId: "846926921210",
+  appId: "1:846926921210:web:ffa2209deb7e2fd06cf843",
+  measurementId: "G-Q9ERP22VTB",
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./fb";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
